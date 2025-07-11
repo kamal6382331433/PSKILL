4af4602a1c627a2cc5808093a55b0f2cb16d6d66
kamal <kamalclan1234@gmail.com>
2025-07-11 15:10:49 +0530

    completed

---
+++ py lvl 4/consecutive same characters.py
@@ -0,0 +1,19 @@
+def find(s):
+    n = len(s)
+    k = []
+    i = 0
+
+    while i < n:
+        count = 1
+        j = i + 1
+        while j < n and s[i] == s[j]:
+            count += 1
+            j += 1
+        k.append(count)
+        i = j
+
+    return max(k)
+
+s = input("Enter: ")
+res = find(s)
+print(res)

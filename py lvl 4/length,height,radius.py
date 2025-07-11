901885e5f5a257dacaacc6f3399b02de20719c27
kamal <kamalclan1234@gmail.com>
2025-07-11 13:45:50 +0530

    compelted

---
+++ py lvl 4/length,height,radius.py
@@ -0,0 +1,15 @@
+r=int(input())
+h=int(input())
+l=int(input())
+def radius(r):
+    r1=float(3/4*r)
+    return r1
+def height(h):
+    h1=h**3
+    return  h1
+def length(l):
+    l1=int(l*l)
+    return l1
+print(radius(r))
+print(height(h))
+print(length(l))
